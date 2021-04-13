64b2e58ccf1257fb0d4fb516a842cf31cc5fb6bd
Chanuka Lakshan <chanukalakshan92@gmail.com>
2021-04-13 16:51:56 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { Contact } from "./components/contacts/Contact"
+import { Navbar } from "./components/Navbar"
 import './App.css';
+import { Provider } from "react-redux";
+import store from "./store";
 
-function App() {
+
+const App =() => {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+        <h1>
+          <Navbar/>    
+          <Contact/>
+        </h1>
+  
     </div>
+    </Provider>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div className="container">
+            <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+            <div className="container">
+            <a class="navbar-brand" href="#">Contact Book</a>
+
+             <a className="btn btn-light ml-auto">Create contact</a>
+            </div>
+            </nav>     
+        </div>
+    )
+}
